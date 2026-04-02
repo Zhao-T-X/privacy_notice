bd6fd968aa3e602e136eba277df6732f0df22785
T.X <tianxin1zhao@gmail.com>
2026-04-02 09:00:48 +0800

    Update process.js

--- process.js
+++ process.js
@@ -28,8 +28,9 @@ async function run() {
 
     // 读取 HTML
     const html = fs.readFileSync(inputFilename, 'utf8');
+    console.error(html)
     const dom = new JSDOM(html);
-    console.error(dom)
+    
     const document = dom.window.document;
 
     // --- 1. 提取日期 ---
